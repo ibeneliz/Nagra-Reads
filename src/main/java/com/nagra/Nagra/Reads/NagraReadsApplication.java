e20b72f5967b56368dcccaf2c5353016f7d19f53
Ibene Liz Mathew <ibeneliz@gmail.com>
2024-07-17 10:48:53 +0530

    Initial Commit

---
+++ src/main/java/com/nagra/Nagra/Reads/NagraReadsApplication.java
@@ -0,0 +1,13 @@
+package com.nagra.Nagra.Reads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NagraReadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NagraReadsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nagra/Nagra/Reads/NagraReadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagra.Nagra.Reads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NagraReadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
