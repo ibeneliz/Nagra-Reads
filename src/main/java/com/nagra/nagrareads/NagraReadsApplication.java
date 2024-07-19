8083cc6813d6c6c9ee66e385ef860e40ba124d87
Ibene Liz Mathew <ibeneliz@gmail.com>
2024-07-19 21:44:16 +0530

    Added Rate Limiting with Bucket4j

--- src/main/java/com/nagra/nagrareads/NagraReadsApplication.java
+++ src/main/java/com/nagra/nagrareads/NagraReadsApplication.java
@@ -2,12 +2,13 @@ package com.nagra.nagrareads;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class NagraReadsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NagraReadsApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(NagraReadsApplication.class, args);
+    }
 }
--- src/test/java/com/nagra/nagrareads/NagraReadsApplicationTests.java
+++ src/test/java/com/nagra/nagrareads/NagraReadsApplicationTests.java
@@ -1,13 +1,9 @@
 package com.nagra.nagrareads;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class NagraReadsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
