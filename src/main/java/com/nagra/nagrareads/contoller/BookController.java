abfac44a8946f9cce8700913065a08f0fb65e2d0
Ibene Liz Mathew <ibeneliz@gmail.com>
2024-07-19 08:55:00 +0530

    Swagger Api Docs added.
    http://localhost:8080/v3/api-docs
    http://localhost:8080/swagger-ui/index.html#/book-controller/addBook

--- src/main/java/com/nagra/nagrareads/contoller/BookController.java
+++ src/main/java/com/nagra/nagrareads/contoller/BookController.java
@@ -2,6 +2,11 @@ package com.nagra.nagrareads.contoller;
 
 import com.nagra.nagrareads.model.Book;
 import com.nagra.nagrareads.service.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,11 +20,32 @@ public class BookController {
     @Autowired
     private BookService bookService;
 
+
+    @Operation(summary = "Get all book")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Returns the books available in store",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Book.class))}),
+            @ApiResponse(responseCode = "401", description = "Authentication failed. Please check the request header!",
+                    content = @Content),
+            @ApiResponse(responseCode = "403", description = "Authorization failed. Please check if you have access!",
+                    content = @Content)})
     @GetMapping
     public List<Book> getAllBooks() {
         return bookService.getAllBooks();
     }
 
+    @Operation(summary = "Add a new book with its information")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Added the book",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Book.class))}),
+            @ApiResponse(responseCode = "400", description = "Bad Request. Please check the request body!",
+                    content = @Content),
+            @ApiResponse(responseCode = "401", description = "Authentication failed. Please check the request header!",
+                    content = @Content),
+            @ApiResponse(responseCode = "403", description = "Authorization failed. Please check if you have access!",
+                    content = @Content)})
     @PostMapping
     public ResponseEntity<Book> addBook(@RequestBody Book book) {
         Book savedBook = bookService.saveBook(book);
